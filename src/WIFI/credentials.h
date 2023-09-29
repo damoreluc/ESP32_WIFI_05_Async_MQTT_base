ce06e985156ead99cc750d54dec4837186419a41
damoreluc <damore.luca@isisbassafriulana.edu.it>
2023-09-29 22:54:26 +0200

    credentials

--- src/WIFI/credentials.h
+++ src/WIFI/credentials.h
@@ -2,10 +2,7 @@
 #define _CREDENTIALS_H
 
 // to be replaced with your WiFi network credentials
-// const char* ssid = "YOUR_WIFI_SSID";
-// const char* password = "YOUR_WIFI_PASSWORD";
-
-#define WIFI_SSID "casa"
-#define WIFI_PASSWORD "crapapelatala"
+#define WIFI_SSID "YOUR_WIFI_SSID";
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD";
 
 #endif
