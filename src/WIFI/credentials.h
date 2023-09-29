d2542a190a795649cc70d33282701cd04401ea20
damoreluc <damore.luca@isisbassafriulana.edu.it>
2023-09-29 22:59:06 +0200

    clean up credentials.h

--- src/WIFI/credentials.h
+++ src/WIFI/credentials.h
@@ -2,7 +2,7 @@
 #define _CREDENTIALS_H
 
 // to be replaced with your WiFi network credentials
-#define WIFI_SSID "YOUR_WIFI_SSID";
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD";
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 
 #endif
