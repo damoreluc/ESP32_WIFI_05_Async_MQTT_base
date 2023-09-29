815dd5d0f002d8706d8f5b6f79cb5f6a42f4f9f4
damoreluc <damore.luca@isisbassafriulana.edu.it>
2023-09-29 23:05:52 +0200

    rm credentials

--- src/WIFI/credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CREDENTIALS_H
-#define _CREDENTIALS_H
-
-// to be replaced with your WiFi network credentials
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
-
-#endif
